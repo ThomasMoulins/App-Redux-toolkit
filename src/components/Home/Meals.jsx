b04c06fd11a3c7ad8e5e41b5f5d704a8fc8a7138
ThomasMoulins <13thomasmoulins@gmail.com>
2024-11-19 12:03:30 +0100

    MOVE modal and button to component

--- src/components/Home/Meals.jsx
+++ src/components/Home/Meals.jsx
@@ -1,24 +1,15 @@
-import { useState } from "react";
-import useToggle from "./useToggle";
-import Modal from "./Modal";
 import Fetchmeals from "./Fetchmeals";
+import Mealsbutton from "./Mealsbutton";
 
 const Meals = ({ selectedCategory, meals }) => {
-  const [selectedMeal, setSelectedMeal] = useState(null); // Repas sélectionné pour la modale
-  const [isModalOpen, toggleModal] = useToggle(false);
   const displayMeals = Fetchmeals(selectedCategory, meals);
-
-  const handleOpenModal = (meal) => {
-    setSelectedMeal(meal);
-    toggleModal(); // Ouvre la modale
-  };
 
   return (
     <main className="max-w-7xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {displayMeals.map((meal) => (
         <div
           key={meal.idMeal}
-          className="border rounded-lg p-4 shadow-md bg-white flex flex-col"
+          className="relative border rounded-lg p-4 shadow-md bg-white flex flex-col"
         >
           <img
             src={meal.strMealThumb}
@@ -26,13 +17,7 @@ const Meals = ({ selectedCategory, meals }) => {
             className="w-full h-32 object-cover rounded-md mb-2"
           />
           <h2 className="text-lg font-semibold mb-4">{meal.strMeal}</h2>
-          <button
-            onClick={() => handleOpenModal(meal)}
-            className="mt-auto px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          >
-            Ajouter au calendrier
-          </button>
-          {isModalOpen && <Modal onClose={toggleModal} meal={selectedMeal} />}
+          <Mealsbutton meal={meal} />
         </div>
       ))}
     </main>
---
+++ src/components/Home/Mealsbutton.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaPlus } from "react-icons/fa";
+import { MdOutlineAddShoppingCart } from "react-icons/md";
+import Modal from "./Modal";
+import useToggle from "./useToggle";
+
+const Mealsbutton = ({ meal }) => {
+  const [selectedMeal, setSelectedMeal] = useState(null); // Repas sélectionné pour la modale
+
+  const handleOpenModal = (meal) => {
+    setSelectedMeal(meal);
+    toggleModal(); // Ouvre la modale
+  };
+  const [isModalOpen, toggleModal] = useToggle(false);
+
+  return (
+    <>
+      {/* Lien avec loupe en haut à droite */}
+      <Link
+        to={`/recipe/${meal.idMeal}`}
+        className="absolute top-6 right-6 p-2 bg-gray-200 bg-opacity-70 text-gray-800 rounded-full hover:bg-gray-300 flex items-center justify-center"
+      >
+        <FaPlus />
+      </Link>
+      <div className="mt-auto flex">
+        <button
+          onClick={() => handleOpenModal(meal)}
+          className="h-10 px-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 flex items-center justify-center"
+        >
+          Ajouter au calendrier
+        </button>
+        <Link
+          to={``}
+          className="h-10 px-4 ms-auto bg-green-500 text-white rounded-md hover:bg-green-600 flex items-center justify-center"
+        >
+          <MdOutlineAddShoppingCart />
+        </Link>
+      </div>
+      {isModalOpen && <Modal onClose={toggleModal} meal={selectedMeal} />}
+    </>
+  );
+};
+
+export default Mealsbutton;
